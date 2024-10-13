95e8d9beec27023c357a9286a7a35a388d0c46b4
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-13 11:44:43 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You',
+    'The Devil Wears Prada',
+    'Pierce the Veil',
+    'Norma Jean',
+    'The Bled',
+    'Say Anything',
+    'The Midway State',
+    'We Came as Romans',
+    'Counterparts',
+    'Oh, Sleeper',
+    'A Skylit Drive',
+    'Anywhere But Here',
+    'An Old Dog'
+];
+
+
+const sortBands = (bands) => {
+    return bands.sort((a, b) => {
+        const strippedA = a.replace(/^(a |an |the )/i, '').toLowerCase();
+        const strippedB = b.replace(/^(a |an |the )/i, '').toLowerCase();
+        return strippedA.localeCompare(strippedB);
+    });
+};
+
+
+const sortedBands = sortBands(bands);
+const bandList = document.getElementById('bands');
+sortedBands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    bandList.appendChild(li);
+});
